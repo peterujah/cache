00ecad441d300d671c8afce29330a519a606b534
Chigozie peter <ujahchigozie@gmail.com>
2021-12-21 21:52:34 +0800

    Create Cache.php

---
+++ src/Cache.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Pagination - Light, simple  PHP and mysql Hierarchy data and organization chart
+ * This class was heavily inspired by interview question
+ * @author      Peter Chigozie(NG) peterujah
+ * @copyright   Copyright (c), 2021 Peter(NG) peterujah
+ * @license     MIT public license
+ */
+namespace Peterujah\NanoBlock;
+
+/**
+ * Class Cache.
+ */
+class Cache {
+
+}
